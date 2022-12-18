deaa7233b257ac973a8adbaf5a9f694076dedd25
fahadhossainn <fahadshakiblst@gmail.com>
2022-12-18 22:59:02 +0600

    small fix in the navigation. section revealing functionality added using Intersection Observer API

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 "use strict";
-const target = document.querySelector(".header");
+const header = document.querySelector(".header");
+const features = document.querySelector(".features");
+const testimonials = document.querySelector(".testimonials");
+const previews = document.querySelector(".previews");
+const footer = document.querySelector(".footer");
 const navbar = document.querySelector(".navbar");
 const navigation = document.querySelector(".navbar__items");
-const toggle = document.querySelector(".navbar__toggle--label");
+const toggle = document.querySelector(".navbar__toggle");
+const links = document.querySelectorAll(".navbar__link");
+const initialPos = header.getBoundingClientRect().top;
 
 //Changing Navigation Background
-const initialPos = features.getBoundingClientRect().top;
 window.addEventListener("scroll", (e) => {
-  const currPos = features.getBoundingClientRect().top;
+  const currPos = header.getBoundingClientRect().top;
   if (currPos < initialPos) {
     navbar.classList.add("fixed");
   } else {
@@ -15,11 +20,35 @@ window.addEventListener("scroll", (e) => {
   }
 });
 
-//Toggling Mobile Navigation
+// Toggling Mobile Navigation
 toggle.addEventListener("click", (e) => {
-  if (navigation.classList.contains("mobile__menu")) {
+  toggle.classList.toggle("hamburger");
+  navigation.classList.toggle("mobile__menu");
+});
+
+links.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (toggle.classList.contains("hamburger"))
+      toggle.classList.remove("hamburger");
     navigation.classList.remove("mobile__menu");
-  } else {
-    navigation.classList.add("mobile__menu");
-  }
+  });
+});
+
+// Revealing Sections
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  console.log(entry.target);
+  entry.target.classList.remove("section__hidden");
+  observer.unobserve(entry.target);
+};
+
+const observer = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+[features, testimonials, previews, footer].forEach((sec) => {
+  observer.observe(sec);
+  sec.classList.add("section__hidden");
 });
